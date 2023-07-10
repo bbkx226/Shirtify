4f6d3e7d93b8ad7f6fc65db8e84950867dfec773
Brandon Ban Kai Xian <bbkx226@gmail.com>
2023-07-10 23:40:39 +0800

    Implemented backend

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -48,6 +48,21 @@ const Customizer = () => {
 
     try {
       // call our backend to generate an ai image!
+      setGeneratingImg(true)
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json()
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
+
     } catch (error) {
       alert(error)
     } finally {
@@ -77,6 +92,7 @@ const Customizer = () => {
       default:
         state.isLogoTexture = true
         state.isFullTexture = false
+        break;
     }
 
     // after setting the state, activeFilterTab is updated
@@ -144,6 +160,13 @@ const Customizer = () => {
                 handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
+            <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button>
           </motion.div>
         </>
       )}
