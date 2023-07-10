2610068332dbe9935cd6792218e10c97b7cba177
Brandon Ban Kai Xian <bbkx226@gmail.com>
2023-07-11 01:04:59 +0800

    Modified content and images

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,24 +17,26 @@ const Home = () => {
       {snap.intro && (
         <motion.section className='home' {...slideAnimation('left')}>
           <motion.header {...slideAnimation('down')}>
-            <img 
-              src='./threejs.png'
-              alt='logo'
-              className='w-8 h-8 object-contain'
-            />
+            <a href='https://shirtify-me.vercel.app'>
+              <img 
+                src='./shirtify-square.png'
+                alt='logo'
+                className='w-32 h-16 object-contain'
+              />
+            </a>
           </motion.header>
 
           <motion.div className='home-content' {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className='xl:block hidden'/>DO IT.
+                Let's <br className='xl:block hidden'/> Shirtify
               </h1>
             </motion.div>
             
             <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
               <p className='max-w-md font-normal text-gray-600 text-base'>
-                Create your unique and exclusive shirt with our brand-new 3D customization tool. 
-                <strong>Unleash your imagination</strong>{" "} and define your own style.
+                <strong>Unleash your style </strong>with our brand-new 3D customization tool, 
+                crafting <strong>one-of-a-kind </strong> shirts that embody your unique imagination.
               </p>
 
               <CustomButton 
