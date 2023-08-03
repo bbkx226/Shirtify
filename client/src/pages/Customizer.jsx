fcc46bb47004a3d4950bb4d71fdbb09c707eae29
bbkx226 <bbkx226@gmail.com>
2023-08-03 17:04:35 +0800

    Changed Listening Router

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -49,7 +49,7 @@ const Customizer = () => {
     try {
       // call our backend to generate an ai image!
       setGeneratingImg(true)
-      const response = await fetch('https://shirtify-me.vercel.app/api/v1/dalle', {
+      const response = await fetch('https://shirtify-me.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
